5badba39e99f26641e04850221817452a743e745
BachErik <75324423+BachErik@users.noreply.github.com>
2022-11-12 15:22:13 +0100

    I am dump and stupid

--- src/main/java/de/bacherik/languagesystem/language/Language.java
+++ src/main/java/de/bacherik/languagesystem/language/Language.java
@@ -2,7 +2,6 @@ package de.bacherik.languagesystem.language;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import de.niklas.nikapi.spigot.config.YAMLConfig;
 
 import java.io.*;
 import java.util.Map;
@@ -10,13 +9,11 @@ import java.util.Map;
 public class Language {
 
     private final String name;
-    private static String acronym;
 
     private JsonObject jsonObject;
 
     public Language(String name, String acronym) {
         this.name = name;
-        Language.acronym = acronym;
         try {
             this.jsonObject = (new JsonParser()).parse(new FileReader("languages\\" + acronym + "\\translations.json")).getAsJsonObject();
         } catch (Exception exception) {
@@ -56,14 +53,5 @@ public class Language {
 
     public String getName() {
         return this.name;
-    }
-
-    public void addEntry(String key, String value) {
-        try {
-            YAMLConfig config = new YAMLConfig("plugins/languages/" + acronym + ".yml");
-            config.addEntry(key, value);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
